44cdda27b96a8182e91ff18cde8e7037a383db22
Ridwan Mohamed <Ridwann04@hotmail.com>
2024-01-14 20:09:39 +0100

    heyy

--- output/exercise.9.cpp
+++ output/exercise.9.cpp
@@ -2,9 +2,47 @@
 //  from the command line and then check if the number is prime or not.
 #include <iostream>
 #include <cstdlib>
+#include <cmath>
 
-int main()
+constexpr int PRIMENMR[] = {0, 1};
+
+static bool isprime(int num) // primenumber function
 {
+    bool prime = true;
+    if (num <= PRIMENMR)
+    {
+        prime = false;
+    }
+    else
+    {
+
+        if (num > PRIMENMR[1])
+        {
+            const int max_number = std::ceil(std::sqrt(num));
+
+            for (int i = 2; i <= max_number; i++)
+            {
+                if (num % i == 0)
+                {
+                    prime = false;
+                    break;
+                }
+            }
+        }
+    }
+    return prime;
+}
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        std::cout << "Inavalid numbers" << std::endl;
+        exit(1);
+    }
+
+    int number = std::stoi(argv[1]);
+
+    std::cout << number << " is " << (isprime(number) ? "" : "not ") << "prime!" << std::endl;
 
     return 0;
 }
